cc51c26a3df7bb915aeb4601d9a7cf42cb575d38
“Poojanmodi” <modipoojan444@gmail.com>
2021-11-05 20:05:04 +0530

    Option check feature added

--- src/com/snake_ladder/Snake.java
+++ src/com/snake_ladder/Snake.java
@@ -10,9 +10,29 @@ public class Snake {
 		
 		System.out.println("The current position of player is "+ player_position);
 		
-		double dice = Math.floor((Math.random()*10)%6+1); 
+		int dice = (int)Math.floor((Math.random()*10)%6+1); 
 		System.out.println("The dice number is "+dice);
-
+		
+		int option = (int)Math.floor((Math.random()*10)%3);
+		
+		switch(option) {
+		
+		case 0:
+			System.out.println("No Play, Roll Aagain");
+			break;
+			
+		case 1:
+			System.out.println("Bravo! You found a ladder");
+			player_position = player_position + dice;
+			break;
+		
+		case 2:
+			System.out.println("Snake!Go backsteps, shown on dice");
+			player_position = player_position - dice;
+			break;
+		}
+		
+		System.out.println("The current position of player is "+ player_position);
 	}
 
 }
