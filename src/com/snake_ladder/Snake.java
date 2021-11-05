c79e115a4106bcf9a72d37a854994b4b413df080
“Poojanmodi” <modipoojan444@gmail.com>
2021-11-05 15:08:46 +0530

    Single player added at position 0

---
+++ src/com/snake_ladder/Snake.java
@@ -0,0 +1,15 @@
+package com.snake_ladder;
+
+public class Snake {
+
+	public static void main(String[] args) {
+		
+		int player_position = 0;
+		
+		System.out.println("Lets start the game");
+		
+		System.out.println("The current position of player is "+ player_position);
+
+	}
+
+}
