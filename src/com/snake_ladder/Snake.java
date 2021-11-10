e68a24fe41e70db8e8ce00d12b158048dc657eae
“Poojanmodi” <modipoojan444@gmail.com>
2021-11-10 07:30:07 +0530

    2 Users added sucessfully

--- src/com/snake_ladder/Snake.java
+++ src/com/snake_ladder/Snake.java
@@ -4,49 +4,86 @@ public class Snake {
 
 	public static void main(String[] args) {
 		
-		int player_position = 0;
-		int dice_roll_time = 0;
+
+		int position = 0;
+		int position2 = 0;
+		System.out.println(" Welcome player!! ");
 		
-		System.out.println("Lets start the game");
 		
-		System.out.println("The current position of player is "+ player_position);
-	
-		while(player_position <100) {
-		int dice = (int)Math.floor((Math.random()*10)%6+1); 
-		System.out.println("The dice number is "+dice);
+		int dice;
+		int option;
+		int roll = 0;
+		int turn = 0;
+		int oldTurn = 0;
 		
-		int option = (int)Math.floor((Math.random()*10)%3);
 		
-		switch(option) {
-		
-		case 0:
-			System.out.println("No Play, Roll again");
-			break;
+		while (position < 100 && position2 < 100) {
 			
-		case 1:
-			System.out.println("Bravo! You found a ladder");
-			player_position = player_position + dice;
-			break;
-		
-		case 2:
-			System.out.println("Snake!Go backsteps, shown on dice");
-			player_position = player_position - dice;
-			if(player_position < 0) {
-				player_position = 0;
+			if (turn == 0) 
+				System.out.println(" Player 1 is playing");
+			else
+				System.out.println(" Player 2 is playing");
+			
+			dice = (int)Math.floor( (Math.random() * 10) % 6 + 1); 
+			System.out.println(" The dice was rolled to: " + dice);
+			option = (int)Math.floor(Math.random() * 3); 
+			
+			switch(option) {
+			case 0: 
+				System.out.println(" Player action: nothing");
+				break;
+			case 1: 
+				System.out.println(" Player action: ladder");
+				if (turn == 0) 
+					position += dice;
+					
+				else
+					position2 += dice;
+				
+				if (position > 100) {
+					position -= dice;
+					System.out.println(" Player1 exceeded 100 so not counting");
+				}
+				
+				if (position2 > 100) {
+					position2 -= dice;
+					System.out.println(" Player2 exceeded 100 so not counting");
+				}
+				break;
+			case 2: 
+				System.out.println(" Player action: snake");
+				if (turn != 0) 
+					position -= dice;
+					
+				else
+					position2 -= dice;
+				
+				
+				if (position < 0) {
+					position = 0;
+					System.out.println(" Player1 reset to 0");
+				}
+				
+				if (position2 < 0) {
+					position2 = 0;
+					System.out.println(" Player2 reset to 0");
+				}
+				
+				turn = (turn + 1) % 2;
+				break;
+							
 			}
-			break;
+			
+			if (oldTurn == 0)
+				System.out.println(" Current position for Player1: " + position + "\n");
+			else
+				System.out.println(" Current position for Player2: " + position2 + "\n");
+			roll++;
+			oldTurn = turn;
 		}
 		
-		if(player_position > 100) {
-			player_position = 100;
-		}
-		System.out.println("The current position of player is "+ player_position);
-		dice_roll_time++;
-		
-		}
-		System.out.println("You have completed the game in "+dice_roll_time+" dice rolls");
+		System.out.println(" Congratulations Player" + (turn + 1) + "!!");
+		System.out.println(" We took " + roll + " rolls to win the game!!");
 	}
-		
-		
 
 }
